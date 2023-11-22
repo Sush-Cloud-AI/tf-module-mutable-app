24ac59ea6f21685428e1cba1ba02b54ac4eb1591
Sush <sushcloud12@gmail.com>
2023-11-22 00:09:02 +0000

    i

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,8 @@ resource "aws_spot_instance_request" "spot" {
   instance_type = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment = true
   #spot_type = "one-time"
+  vpc_security_group_ids = [aws_security_group.allows_app.id]
+  subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
@@ -16,5 +18,17 @@ resource "aws_instance" "app" {
     count = var.OD_INSTANCE_COUNT
     ami = data.aws_ami.ami.image_id
     instance_type = var.OD_INSTANCE_TYPE
-  
+    vpc_security_group_ids = [aws_security_group.allows_app.id]
+    subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+
+ }
+
+# Creates EC2 TAGS and attahces to the SERVER
+resource "aws_ec2_tag" "app_tags" {
+  count  = local.INSTANCE_COUNT
+  resource_id  = element(local.INSTANCE_IDS, count.index)
+  key          = "Name"
+  value        = "${var.COMPONENT}-${var.ENV}"
 }
+
+# if the forntend comp create on Public subnet. backend component on public subnet
--- vars.tf
+++ vars.tf
@@ -2,16 +2,12 @@
 variable "ENV" {}
 
 variable "COMPONENT" {}
-variable "SPOT_INSTANCE_TYPE" {
-  
-}
-variable "SPOT_INSTANCE_COUNT" {
-  
-}
+variable "SPOT_INSTANCE_TYPE" {}
+variable "SPOT_INSTANCE_COUNT" {}
 
-variable "OD_INSTANCE_TYPE" {
-  
-}
-variable "OD_INSTANCE_COUNT" {
-  
-}
+variable "OD_INSTANCE_TYPE" {}
+
+variable "OD_INSTANCE_COUNT" {}
+
+variable "INTERNAL" {}
+variable "APP_PORT" {}
