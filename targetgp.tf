6242cc07a2732bdcbbf3aa7ef3a496aafa8415c2
Sush <sushcloud12@gmail.com>
2023-11-25 22:05:38 +0000

    i

---
+++ targetgp.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_target_group" "app" {
+  name     = "${var.COMPONENT}-${var.ENV}-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+# attach the tg to the load balancer 
+
+resource "aws_lb_target_group_attachment" "attach_instances" {
+  count = local.INSTANCE_COUNT
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = element(local.INSTANCE_IDS, count.index)
+  port             = 8080
+}
