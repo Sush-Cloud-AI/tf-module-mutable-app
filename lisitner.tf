2f0e02451a672b112bd753319f4c2be76ac78e6b
Sush <sushcloud12@gmail.com>
2023-11-25 23:30:30 +0000

    i

--- lisitner.tf
+++ lisitner.tf
@@ -11,3 +11,23 @@ resource "aws_lb_listener" "private" {
 
   }
 }
+
+# create a rule in the private loadbalancer listner
+
+resource "aws_lb_listener_rule" "app_rule" {
+    count = var.INTERNAL ? 1 : 0
+    listener_arn = aws_lb_listener.private.arn
+    priority = 99
+
+    action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.app.arn
+
+    }
+
+    condition {
+      host_header {
+        values = ["${var.COMPONENT}-${var.ENV}.data.terraform_remote_state.vpc.PRIVATE_HOSTED_ZONE_NAME"]
+      }
+    }
+}
---
+++ route53.tf
@@ -0,0 +1,8 @@
+# Adding app record against the alb
+resource "aws_route53_record" "app_dns" {
+  zone_id = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
+  name    = "${var.COMPONENT}-${var.ENV}"
+  type    = "CNAME"
+  ttl     = 10
+  records = var.INTERNAL ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
+}
