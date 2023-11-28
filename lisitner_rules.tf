c1b23d27ea80eef73756ccf9fa44ca154b04d202
Sush <sushcloud12@gmail.com>
2023-11-28 01:53:45 +0000

    i

--- lisitner_rules.tf
+++ lisitner_rules.tf
@@ -11,7 +11,7 @@ resource "random_integer" "priority" {
 
 resource "aws_lb_listener_rule" "app_rule" {
     count = var.INTERNAL ? 1 : 0
-    listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN
+    listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
     priority = random_integer.priority.result         
 ## we cannot give a fixed priority number, as it has has to be unique for each 
 ## lisitner. so we use random integer fuction to assign 
