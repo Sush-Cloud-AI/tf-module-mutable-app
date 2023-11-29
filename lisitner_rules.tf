7c0f4b3938997366d16be4123f358d666fa10455
Sush <sushcloud12@gmail.com>
2023-11-29 22:58:39 +0000

    i

--- lisitner_rules.tf
+++ lisitner_rules.tf
@@ -1,3 +1,19 @@
+# Creates Listerner For the Public ALB
+
+# This creates the listener and adds to the public ALB( forntend)
+resource "aws_lb_listener" "public" {
+  count             = var.INTERNAL ? 0 : 1
+
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+default_action {
+ 
+      type = "forward"
+      target_group_arn = aws_lb_target_group.app.arn
+  }
+  
+ }
 
 ## Generates a random integer in the range
 resource "random_integer" "priority" {
